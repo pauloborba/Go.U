75999a04ca63f9f3d295badaa7bb85e6cd30ca91
nyancy <gagp@cin.ufpe.br>
2018-06-25 07:49:25 +0900

    [#15] so aceita numeros positivos para as horas

--- gui/GoU-gui/src/app/MelhorHora/hora.service.ts
+++ gui/GoU-gui/src/app/MelhorHora/hora.service.ts
@@ -3,6 +3,14 @@ import { Hora } from './hora';
 export class HoraService {
   horas: Hora[] = [];
   gravar(hora: Hora): Hora {
-     this.horas.push(hora);
+     var result = null;
+
+     if ((!isNaN(hora.hf)) && (!isNaN(hora.hi)) && (!isNaN(hora.mi)) && (!isNaN(hora.mf))) {
+      if (hora.hf>=0 && hora.hi>=0 && hora.mf>=0 && hora.mi>=0) {
+                 this.horas.push(hora);
+                 result = hora;       
+      }
+     }
+     return result;
   }
 }
