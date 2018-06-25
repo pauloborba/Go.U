345a1f07aadc1233b3512ee8439bdaac94415c23
nyancy <gagp@cin.ufpe.br>
2018-06-25 09:12:25 +0900

    [#2] preparacao para implementacao do servidor

--- gui/GoU-gui/src/app/MelhorHora/hora.ts
+++ gui/GoU-gui/src/app/MelhorHora/hora.ts
@@ -1,8 +1,33 @@
 export class Hora {
   user: string;
   local: string;
-  hi;
-  mi;
-  hf;
-  mf;
+  hi: number;
+  mi: number;
+  hf: number;
+  mf: number;
+
+  constructor() {
+  this.remove();
+  }
+
+  remove(): void {
+    this.user = "";
+    this.local = "";
+    this.hi = null;
+    this.hf = null;
+    this.mi = null;
+    this.mf = null;
+  }
+
+  clone(): Hora {
+    var hora: Hora = new Hora();
+    hora.user = this.user;
+    hora.local = this.local;
+    hora.hi = this.hi;
+    hora.hf = this.hf;
+    hora.mi = this.mi;
+    hora.mf = this.mf;
+    return hora;
+  }
+
 }
