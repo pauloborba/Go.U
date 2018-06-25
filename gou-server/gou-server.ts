a728346dc7bb714495e3dde324fc3571c2b75bbb
Lucas Thierry <ltcm@cin.ufpe.br>
2018-06-25 18:54:16 -0300

    [#6]Implementação do servidor de agendamento

--- gou-server/gou-server.ts
+++ gou-server/gou-server.ts
@@ -4,9 +4,13 @@ import bodyParser = require("body-parser");
 import {Hora} from '../gui/GoU-gui/src/app/MelhorHora/hora';
 import {MelhorHora} from './melhorhora';
 
+import { Agendamento } from '../../gui/GoU-gui/src/app/Agendamento/agendamento';
+import { NovoAgendamento } from './novoagendamento';
+
 var app = express();
 
 var horas: MelhorHora = new MelhorHora();
+var agendamento: NovoAgendamento = new NovoAgendamento();
 
 var allowCrossDomain = function(req: any, res: any, next: any) {
     res.header('Access-Control-Allow-Origin', "*");
@@ -17,6 +21,8 @@ var allowCrossDomain = function(req: any, res: any, next: any) {
 app.use(allowCrossDomain);
 
 app.use(bodyParser.json());
+
+//Servidor de melhor hora
 
 app.get('/horas', function (req, res) {
   console.log('GET /horas: ' + req)
@@ -30,6 +36,32 @@ app.post('/hora', function (req: express.Request, res: express.Response) {
     res.send({"success": "A hora foi gravada com sucesso"});
   } else {
     res.send({"failure": "A hora nao pode ser gravada"});
+  }
+})
+
+//Servidor de agendamento
+
+app.get('/novoagen', function (req, res) {
+  res.send(JSON.stringify(agendamento.getAgendamentos()));
+})
+
+app.post('/novoagen', function (req: express.Request, res: express.Response) {
+  var agendamento1: Agendamento = <Agendamento>req.body; //verificar se é mesmo agendamento!
+  agendamento1 = agendamento.criar(agendamento1);
+  if (agendamento1) {
+    res.send({ "success": "O agendamento foi cadastrado com sucesso" });
+  } else {
+    res.send({ "failure": "O agendamento não pode ser cadastrado" });
+  }
+})
+
+app.put('/agendamento', function (req: express.Request, res: express.Response) {
+  var agendamento1: Agendamento = <Agendamento>req.body;
+  agendamento1 = agendamento.atualizar(agendamento1);
+  if (agendamento1) {
+    res.send({ "success": "O agendamento foi atualizado com sucesso" });
+  } else {
+    res.send({ "failure": "O agendamento não pode ser atualizado" });
   }
 })
 
---
+++ gou-server/novoagendamento.ts
@@ -0,0 +1,27 @@
+import {Agendamento} from '../../gui/GoU-gui/src/app/Agendamento/agendamento';
+
+export class NovoAgendamento {
+  agendamentos: Agendamento[] = [];
+
+  criar(agendamento: Agendamento): Agendamento {
+    var result = null;
+    
+      result = new Agendamento();
+      result.copyFrom(agendamento);
+      this.agendamentos.push(result);
+    
+    return result;
+  }
+
+ 
+
+  atualizar(agendamento: Agendamento): Agendamento {
+    var result: Agendamento = this.agendamentos.find(a => a.usuario == agendamento.usuario);
+    if (result) result.copyFrom(agendamento);
+    return result;
+  }
+
+  getAgendamentos(): Agendamento[] {
+    return this.agendamentos;
+  }
+}
